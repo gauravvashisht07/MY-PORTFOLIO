c0947f9d9aeda15d71ba969c9fc362c4cd3d13ce
Gaurav <gs177137@gmail.com>
2025-05-06 17:27:56 +0530

    Your Message

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MY-PORTFOLIO/', // your repo name here
+  base: './' // 👈 Use this for Netlify and local hosting
 })
